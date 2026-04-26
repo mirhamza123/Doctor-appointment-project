cc8c104c6db516e9478e6719efafe88b2a360d9b
mirhamza123 <mirh7169@gmail.com>
2026-04-26 12:31:12 +0500

    Refactor footer links to use NavLink for improved navigation and consistency

--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
@@ -13,9 +14,21 @@ function Footer() {
         <div>
           <h1 className="textxl font-medium mb-5">Company</h1>
           <ul className="flex flex-col gap-2 text-gray-600">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
+            <li>
+              <NavLink to="/" className="hover:text-primary transition">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className="hover:text-primary transition">
+                About Us
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="hover:text-primary transition">
+                Contact Us
+              </NavLink>
+            </li>
             <li>Privacy Policy</li>
           </ul>
         </div>
