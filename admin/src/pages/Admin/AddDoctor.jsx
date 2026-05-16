52f651a91e67be2d08dc421304f15c7366a580db
mirhamza123 <mirh7169@gmail.com>
2026-05-16 12:37:05 +0500

    show password icon

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -13,6 +13,7 @@ const AddDoctor = () => {
   const [fees, setFees] = useState("");
   const [about, setAbout] = useState("");
   const [speciality, setSpeciality] = useState("General physician");
+  const [showPassword, setShowPassword] = useState(false);
   const [degree, setDegree] = useState("");
   const [address1, setAddress1] = useState("");
   const [address2, setAddress2] = useState("");
@@ -121,14 +122,41 @@ const AddDoctor = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor password</p>
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                className="border rounded px-3 py-2"
-                type="password"
-                placeholder="password"
-                required
-              />
+              <div className="relative">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  className="border rounded px-3 py-2 w-full pr-10"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="password"
+                  required
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 cursor-pointer"
+                >
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l1.73 1.73A11.1 11.1 0 0 0 1.5 12c1.97 3.78 5.95 6.75 10.5 6.75 1.4 0 2.74-.26 3.99-.74l2.1 2.1a.75.75 0 1 0 1.06-1.06L3.53 2.47Zm5.55 5.55 1.88 1.88a2.25 2.25 0 0 0 2.84 2.84l1.88 1.88A7.16 7.16 0 0 1 12 17.25c-3.45 0-6.33-2.1-7.5-5.25.7-1.34 1.7-2.5 2.9-3.35Zm4.72 4.72-1.29-1.29a.75.75 0 0 0-1.06 1.06l1.29 1.29a.75.75 0 0 0 1.06-1.06ZM12 6.75c-1.96 0-3.7.82-4.95 2.15l1.42 1.42A3.75 3.75 0 0 1 12 9.75c.5 0 .98.1 1.42.28l1.48 1.48A3.75 3.75 0 0 1 12 6.75Zm0 10.5a7.16 7.16 0 0 1-4.78-1.84l1.46-1.46c.75.4 1.6.65 2.52.65 1.74 0 3.28-.86 4.22-2.18l1.5 1.5A9.58 9.58 0 0 1 12 17.25Z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path d="M12 5.25c-4.55 0-8.53 2.97-10.5 6.75 1.97 3.78 5.95 6.75 10.5 6.75 4.55 0 8.53-2.97 10.5-6.75C20.53 8.22 16.55 5.25 12 5.25Zm0 11.25a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9Zm0-1.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
